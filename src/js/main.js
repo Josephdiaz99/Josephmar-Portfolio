3940398b7f9194304ddea8005a2d3555d56f10dd
Josephmar Diaz <josephmar@diaz.com>
2022-09-02 10:38:29 -0400

    saves

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,6 @@ import '../scss/style.scss'
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
 import typingWord from './functions/typingWords'
-import proyectHover from './functions/proyectHover'
 import contactForm from './functions/contactForm'
 
 document.addEventListener('DOMContentLoaded',()=>{
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 module.exports = {
-  entry: '/dist/src/js/main.js',
+  entry: './src/js/main.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
@@ -10,7 +10,7 @@ module.exports = {
     static: path.resolve(__dirname, 'dist'),
     port: 8080,
     hot: true
-  }, 
+  },
   module: {
     rules: [
       {
